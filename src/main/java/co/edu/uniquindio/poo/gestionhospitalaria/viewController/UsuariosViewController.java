c77175cbb56040ffb44b1d7dde9f0d6f230d8b8d
JuanAndresArboleda <juana.arboledam@uqvirtual.edu.co>
2025-03-12 10:18:17 -0500

    interfaces

---
+++ src/main/java/co/edu/uniquindio/poo/gestionhospitalaria/viewController/UsuariosViewController.java
@@ -0,0 +1,31 @@
+package co.edu.uniquindio.poo.gestionhospitalaria.viewController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+public class UsuariosViewController {
+    @FXML
+    private Text lbl_bienvenido;
+
+    @FXML
+    private AnchorPane ap_usuario;
+
+    @FXML
+    private Text lbl_iniciarComo;
+
+    @FXML
+    private Button btn_paciente;
+
+    @FXML
+    private Button btn_administrador;
+
+
+    @FXML
+    void onPaciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAdministrador(ActionEvent event){
+    }
+}
