bf1cd54f834478ec348b3fee1f444b0b363da12a
JuanAndresArboleda <juana.arboledam@uqvirtual.edu.co>
2025-03-12 18:24:05 -0500

    ESTOY HARTO DE REHACER TODO LO QUE HAGO

--- src/main/java/co/edu/uniquindio/poo/gestionhospitalaria/App.java
+++ src/main/java/co/edu/uniquindio/poo/gestionhospitalaria/App.java
@@ -1,4 +1,22 @@
 package co.edu.uniquindio.poo.gestionhospitalaria;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.AdministradorViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.CitasViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.CrudDoctorViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.CrudPacienteViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.InicioAdministradorViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.InicioPacienteViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.PacienteViewController;
+import co.edu.uniquindio.poo.gestionhospitalaria.viewController.UsuariosViewController;
 public class App {
 }
